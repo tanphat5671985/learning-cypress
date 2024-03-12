40145e1b05dcae52216b9a87398b4dac23dd549d
Phat <tanphat5671985@gmail.com>
2024-03-12 17:04:46 +0700

    update HeaderTest.js file

--- cypress/tests/components/HeaderTest.js
+++ cypress/tests/components/HeaderTest.js
@@ -4,16 +4,17 @@ describe ('Header Component Test', () => {
     
     const BRAND_TEXT = 'PRODUCT STORE';
     let headerComp;
+    //run for each 'it' block
     beforeEach(()=>{
         cy.visit('/');
         headerComp= new HeaderComponent();
     })
-    
     it('Test for branch logo', ()=>{
         headerComp.brandLogoImg().should('be.visible')
         headerComp.brandLogo().should('contain.text', BRAND_TEXT)
 
     });
+
     it('Test for header menu details', ()=>{
         const expectedMenuDetails = [
             {"text":"Home (current)","href":"index.html"},
@@ -23,7 +24,7 @@ describe ('Header Component Test', () => {
             {"text":"Log in","href":"#"},
             {"text":"Sign up","href":"#"}
         ]
-        
+        //verify title of menubar
         headerComp.getMenuDetails().then(actualMenuDetail => {
             cy.log(JSON.stringify(actualMenuDetail))
             cy.wrap('').then(()=>{
