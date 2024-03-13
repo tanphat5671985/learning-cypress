f52e3ef6dfac601fd5f77f6f5b843fd70fe8ea54
Phat <tanphat5671985@gmail.com>
2024-03-13 15:35:00 +0700

    done unit 20 product category

--- cypress/support/HomePageAPI.js
+++ cypress/support/HomePageAPI.js
@@ -1,8 +1,15 @@
 export class HomePageAPI {
     
     static getHomePageProducts(){
+        this._waitForEntriesRequest();
+        return cy.get('@entries').then(entries => entries.response.body.Items)
+    }
+
+    static waitForHomePageLoaded(){
+        this._waitForEntriesRequest();
+    }
+    static _waitForEntriesRequest(){
         cy.intercept('/entries').as('entries')
         cy.wait('@entries')
-        return cy.get('@entries').then(entries => entries.response.body.Items)
     }
 }
